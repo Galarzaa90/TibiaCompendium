062902ea79618b5411e07135e83c4ea609c9fb35
Allan Galarza <allan.galarza@gmail.com>
2016-04-16 23:59:14 -0700

    Show section on main screen *Non functional buttons yet *Guild search section coming soon

--- app/src/main/java/com/galarza/tibiacompendium/MainActivity.java
+++ app/src/main/java/com/galarza/tibiacompendium/MainActivity.java
@@ -1,17 +1,16 @@
 package com.galarza.tibiacompendium;
 
-import android.os.Bundle;
-import android.view.View;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import android.app.Activity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
@@ -135,8 +134,6 @@ public class MainActivity extends AppCompatActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            html = (EditText)rootView.findViewById(R.id.html);
 
 
 
