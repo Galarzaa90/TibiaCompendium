6630c1e7c8ad7a751086480a16cca3504a291a0a
Allan Galarza <allan.galarza@gmail.com>
2016-10-22 09:48:10 -0700

    Activity retains fragment on screen rotate, support for Android 7.1

--- app/src/main/java/com/galarza/tibiacompendium/MainActivity.java
+++ app/src/main/java/com/galarza/tibiacompendium/MainActivity.java
@@ -3,6 +3,7 @@ package com.galarza.tibiacompendium;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -26,11 +27,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container,HomeFragment.newInstance())
-                .addToBackStack(null)
-                .commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
+        if(savedInstanceState == null){
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container,HomeFragment.newInstance())
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 
     @Override
